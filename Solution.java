a68bc2c910f8340758ca50a2a5cc07911a162436
mensalda <mensalda@mail.ru>
2020-09-12 15:24:37 +0500

    first commit

---
+++ Solution.java
@@ -0,0 +1,10 @@
+/* 
+Немедленно в печать
+*/
+public class Solution {
+    // напишите тут ваш код
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, Amigo!");
+    }
+}
