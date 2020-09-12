ca0bc3c691ff386e3cae0359b1069a1c464b9741
mensalda <mensalda@mail.ru>
2020-09-12 15:34:04 +0500

    second commit

--- Solution.java
+++ Solution.java
@@ -5,6 +5,6 @@ public class Solution {
     // напишите тут ваш код
     
     public static void main(String[] args) {
-        System.out.println("Hello, Amigo!");
+        System.out.println("Hello, Mike!");
     }
 }
