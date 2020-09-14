7625767dba94e08fd5e0f63c39c1b53cb7b01e3e
mensalda <45210815+mensalda@users.noreply.github.com>
2020-09-14 19:24:18 +0500

    Commit in GitHub

--- Solution3.java
+++ Solution3.java
@@ -5,6 +5,6 @@ public class Example {
     // напишите тут ваш код
     
     public static void main(String[] args) {
-        System.out.println("Hello, Mike!");
+        System.out.println("Hello, Mike!!!!");
     }
 }
