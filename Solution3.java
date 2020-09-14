f46b19620a5aeaa92580defa6da4d7ff9ba277bb
mensalda <mensalda@mail.ru>
2020-09-14 18:16:06 +0500

    Change Bye to Hello

--- Solution3.java
+++ Solution3.java
@@ -6,6 +6,6 @@ public class Example {
     
     public static void main(String[] args) {
         System.out.println("Mike!");
-	System.out.println("Bye, Mike!");
+	System.out.println("Hello, Mike!");
     }
 }
